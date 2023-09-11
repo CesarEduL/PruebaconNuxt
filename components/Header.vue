2a2bd1165df432cdf8628f204c79fdb63c41b915
Cesar Eduardo Lizama Camero <U20201545@utp.edu.pe>
2023-09-11 16:53:17 -0500

    Agregado un Login, pero falta registro de sesión

--- components/Header.vue
+++ components/Header.vue
@@ -1,45 +1,47 @@
 <template>
-    <nav>
-        <div class="nav-bar">
-            <i class="bx bx-menu sidebarOpen"></i>
-            <span class="logo navLogo"><nuxt-link to="/">CompuPost</nuxt-link></span>
+  <nav>
+    <div class="nav-bar">
+      <i class="bx bx-menu sidebarOpen"></i>
+      <span class="logo navLogo"><nuxt-link to="/">CompuPost</nuxt-link></span>
 
-            <div class="menu">
-                <div class="logo-toggle">
-                    <span class="logo"><nuxt-link to="/">CompuPost</nuxt-link></span>
-                    <i class="bx bx-x siderbarClose"></i>
-                </div>
-
-                <ul class="nav-links">
-                    <li><nuxt-link to="/">Home</nuxt-link></li>
-                    <li><nuxt-link to="/Blog">Blog</nuxt-link></li>
-                    <li><nuxt-link to="/Contacto">Contacto</nuxt-link></li>
-                </ul>
-            </div>
-
-            <div class="darkLight-searchBox">
-                <div class="dark-light">
-                    <i class="bx bx-moon moon"></i>
-                    <i class="bx bx-sun sun"></i>
-                </div>
-
-                <div class="searchBox">
-                    <div class="searchToggle">
-                        <i class="bx bx-x cancel"></i>
-                        <i class="bx bx-search search"></i>
-                    </div>
-
-                    <div class="search-field">
-                        <input type="text" placeholder="Search..." />
-                        <i class="bx bx-search"></i>
-                    </div>
-                </div>
-            </div>
+      <div class="menu">
+        <div class="logo-toggle">
+          <span class="logo"><nuxt-link to="/">CompuPost</nuxt-link></span>
+          <i class="bx bx-x siderbarClose"></i>
         </div>
-    </nav>
+
+        <ul class="nav-links">
+          <li><nuxt-link to="/">Home</nuxt-link></li>
+          <li><nuxt-link to="/Blog">Blog</nuxt-link></li>
+          <li><nuxt-link to="/Contacto">Contacto</nuxt-link></li>
+        </ul>
+      </div>
+
+      <div class="darkLight-searchBox">
+        <div class="dark-light">
+          <i class="bx bx-moon moon"></i>
+          <i class="bx bx-sun sun"></i>
+        </div>
+
+        <div class="searchBox">
+          <div class="searchToggle">
+            <i class="bx bx-x cancel"></i>
+            <i class="bx bx-search search"></i>
+          </div>
+
+          <div class="search-field">
+            <input type="text" placeholder="Search..." />
+            <i class="bx bx-search"></i>
+          </div>
+        </div>
+      </div>
+       <nuxt-link to="/Login"><button class="login-btn">LOG IN</button></nuxt-link>
+    </div>
+  </nav>
 </template>
 
-<script setup>
+<script>
+
 </script>
 
 <style scoped></style>
--- nuxt.config.js
+++ nuxt.config.js
@@ -1,7 +1,7 @@
 export default {
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
-    title: "pagina",
+    title: "ComandNews",
     htmlAttrs: {
       lang: "en",
     },
@@ -11,7 +11,13 @@ export default {
       { hid: "description", name: "description", content: "" },
       { name: "format-detection", content: "telephone=no" },
     ],
-    link: [{ rel: "icon", type: "image/x-icon", href: "/logoempresacircularico.ico" }],
+    link: [
+      {
+        rel: "icon",
+        type: "image/x-icon",
+        href: "/logoempresacircularico.ico",
+      },
+    ],
   },
 
   // Global CSS: https://go.nuxtjs.dev/config-css
@@ -20,6 +26,7 @@ export default {
     "@/assets/css/main.css",
     "@/assets/css/footer.css",
     "@/assets/css/publicaciones.css",
+    "@/assets/css/login.css",
     // Boxicons
     "boxicons/css/boxicons.min.css",
     // Bootstrap
@@ -29,7 +36,10 @@ export default {
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
   plugins: [
     { src: "~/plugins/navbar.js", mode: "client" },
-    { src: "~/node_modules/bootstrap/dist/js/bootstrap.bundle.min.js", mode: "client" },
+    {
+      src: "~/node_modules/bootstrap/dist/js/bootstrap.bundle.min.js",
+      mode: "client",
+    },
   ],
 
   // Auto import components: https://go.nuxtjs.dev/config-components
@@ -42,7 +52,43 @@ export default {
   modules: [
     // https://go.nuxtjs.dev/axios
     "@nuxtjs/axios",
+    "@nuxtjs/firebase",
   ],
+
+  firebase: {
+    config: {
+      apiKey: process.env.VUE_APP_FIREBASE_KEY,
+      authDomain: "htmlprojects-adc57.firebaseapp.com",
+      projectId: "htmlprojects-adc57",
+      storageBucket: "htmlprojects-adc57.appspot.com",
+      messagingSenderId: "913985629546",
+      appId: "1:913985629546:web:63430029f7d1ca03678913",
+      measurementId: "G-9T8TQS78P0",
+    },
+    services: {
+      auth: true,
+      firestore: true,
+      storage: true,
+      analytics: true,
+      performance: true,
+    },
+  },
+
+  auth: {
+  persistence: 'local', // default
+  initialize: {
+    onAuthStateChangedMutation: 'ON_AUTH_STATE_CHANGED_MUTATION',
+    onAuthStateChangedAction: 'onAuthStateChangedAction',
+    subscribeManually: false,
+  },
+  ssr: false, // default
+  emulatorPort: 9099,
+  emulatorHost: 'http://localhost',
+},
+
+  env: {
+    VUE_APP_FIREBASE_KEY: process.env.VUE_APP_FIREBASE_KEY,
+  },
 
   // Axios module configuration: https://go.nuxtjs.dev/config-axios
   axios: {
